0a80fdcf9b2e344403da36ba93ec0566a5c96d47
Josh Gubler <josh.gubler@gmail.com>
2021-07-15 16:58:54 -0600

    better sync algorithm

--- src/sync.sh
+++ src/sync.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
-aws s3 sync ../data/ s3://atru5-trailer-data
-sleep 60
+#!/bin/bash -e
+
+while true
+do
+	files=`ls ../data/ | grep '^20'`
+	sleep 10 #to ensure files are done writing
+	for file in $files
+	do
+		aws s3 cp ../data/$file s3://trailer-monitor-rpi/
+		mv ../data/$file ../data-past/
+	done
+done
+
